5fd47be483c4399917cd375b0a777ad6fc355195
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-10-23 15:37:08 -0400

    [1] added index and complete first problem

--- 1-tripler.js
+++ 1-tripler.js
@@ -9,5 +9,19 @@ tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
 
 function tripler(array) {
-  
+    const result = [];
+    // return a new array
+    // iterate array that is passed in
+    // multiply each element by 3
+    // push new element into result array
+    // return rresult x
+    console.log ('Inside of this function')
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        let multi = num *3;
+        result.push(multi);   
+    }
+
+    
+  console.log(tripler([1,2,3]));
 }
