f7dcfab236023657f8506cd14f769c9003adcd55
swolepenguin <ashtonm223@gmail.com>
2020-10-23 17:05:10 -0400

    muscles

--- 2-odd-range.js
+++ 2-odd-range.js
@@ -9,5 +9,32 @@ oddRange(6); // => [ 1, 3, 5 ]
 ******************************************************************************/
 
 function oddRange(end) {
+    const result = [];
+    //return an array
+    //edge casees: if given a negative numebr do nothing with that number 
+    if (end < 0) {
+        return result; 
+    } else{
+        for (let i = 1; i <= end; i++) {
+            if (i % 2 === 0) {
+                continue
+            }else{
+                result.push(i);
+            }
+        }
+    }
+       // for(let i = 1; i= end; i+=)
 
+    console.log
+    console.log
+    console.log
+
+
+
+
+    // set up a for loop and start the loop at 1 
+    //iterate to check if each number has a remainder of 0
+    // we can also iterate by skipping over numbers
+    
+    return result;
 }
