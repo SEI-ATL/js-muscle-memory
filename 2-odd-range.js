b34fd6a62514c23e574ed9211d272c05ce50131c
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-10-23 17:04:39 -0400

    [js files] Went through 3 js exercises.

--- 2-odd-range.js
+++ 2-odd-range.js
@@ -9,5 +9,21 @@ oddRange(6); // => [ 1, 3, 5 ]
 ******************************************************************************/
 
 function oddRange(end) {
-
+  oddArr = [];
+  if (end < 0) {
+    return "Only positive numbers allowed";
+  } else {
+    for (let i = 1; i <= end; i+=2) {
+    //   if (i % 2 !== 0) {
+        oddArr.push(i);
+    //   }
+    }
+  }
+  return oddArr;
 }
+
+//Two methods listed above. 
+
+console.log(oddRange(13));
+console.log(oddRange(-90));
+
