8befdc16b3b8f9aed0fe83ed770611f938a22f6c
Your Name <you@example.com>
2020-10-23 17:06:30 -0400

    followed along

--- 3-is-prime.js
+++ 3-is-prime.js
@@ -12,6 +12,22 @@ isPrime(2017); // => true
 ***************************************************************************/
 
 function isPrime(number) {
-
+    // start at 2 and check to see if the number is divisible
+    // if divisible by any number other than 1, then return false
+    // otherwise, we should return true
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) { // 10 % 2 = 0
+            return false
+        }
+    }
+    return true;
 }
-
+console.log(isPrime(2))
+console.log(isPrime(2)); // => true
+console.log(isPrime(10)); // => false
+console.log(isPrime(11)); // => true
+console.log(isPrime(9)); // => false
+console.log(isPrime(2017)); // => true
+// return a true or false statement depending on if the number is prime or not
+// use if loop to determine if it's prime
+// figure out what formula i need to determine if a number is prime or not
