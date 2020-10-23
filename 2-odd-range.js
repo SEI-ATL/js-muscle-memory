beff74e44b51dffa6ba0f4d896294fd54066fd45
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-10-23 16:44:22 -0400

    [js] add code for function

--- 2-odd-range.js
+++ 2-odd-range.js
@@ -9,5 +9,15 @@ oddRange(6); // => [ 1, 3, 5 ]
 ******************************************************************************/
 
 function oddRange(end) {
-
+    const result = [];
+    if (end <= 0){
+        return result;
+    } else {
+        for (let i = 1; i <= end; i += 2){
+            result.push(i);
+        }
+    }
+    return result;
 }
+
+console.log(oddRange(5))
