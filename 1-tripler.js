b832ccfd0d7e41a132c61088b64136b6ccee0195
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-10-23 15:36:01 -0400

    Tripler function complete

--- 1-tripler.js
+++ 1-tripler.js
@@ -8,6 +8,27 @@ tripler([1,2,3]); // => [ 3, 6, 9 ]
 tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
 
+// understand the problem
+// what the problem is asking me to return
+// what do i need to solve the problem
+
 function tripler(array) {
-  
+    const result = [];
+
+    //return a new array
+    // iterate through the passed in array
+    console.log('Inside of tripler function');
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        let multiple = num * 3;
+        result.push(multiple);
+    }
+    // multiply each element by 3
+    // push new element into result
+    // return result
+
+    return result;
 }
+console.log(tripler([1,2,3]));
+console.log(tripler([4, 1, 7]));
+
