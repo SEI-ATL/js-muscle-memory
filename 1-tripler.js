f9fe7665987359bfe7f02966ec5a022b06dc81b2
Richard Leung <michelleli@MMac.local>
2020-10-23 15:39:37 -0400

    [1-tripler.js] Add function to file

--- 1-tripler.js
+++ 1-tripler.js
@@ -9,5 +9,20 @@ tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
 
 function tripler(array) {
-  
-}
+    const result = [];
+    //return a new array
+    // iterate through the array
+    console.log('Inside of tripler function');
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        let multiple = 3 * num;
+        result.push(multiple);
+    }
+    //multiple each element by 3
+    //push that element into my result
+    //return result
+    return result;
+};
+
+console.log(tripler([1, 2, 3]));
+console.log(tripler([4, 1, 7]));
