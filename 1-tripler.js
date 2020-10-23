d233c9b1b948c4065d31ea3cb61c1d1610c14b75
MusicMan <musicman@MacBook-Pro.fios-router.home>
2020-10-23 15:40:41 -0400

    [multiplier] added the triple function

--- 1-tripler.js
+++ 1-tripler.js
@@ -1,13 +1,18 @@
-/*******************************************************************************
-Write a function tripler(array) that takes in an array and returns a new array
-containing 3 times every element of the original array.
 
-Examples:
+function tripler(array) {}
+const result = [];
 
-tripler([1,2,3]); // => [ 3, 6, 9 ]
-tripler([4, 1, 7]); // => [ 12, 3, 21 ]
-*******************************************************************************/
-
-function tripler(array) {
-  
+for (let i = 0; i < array.length; i++) {
+    let num = array[1]; 
+    let multiple = num * 3;
+ result.push(multiple); 
+//  3, 6, 9
 }
+return result;
+}
+
+console.log(tripler([1,2,3]));
+console.log(tripler([4, 1, 7]));
+
+
+
