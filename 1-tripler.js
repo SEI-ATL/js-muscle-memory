b4cc4ba1fe4c5ecce7db7243cabafc7b22fbf184
Your Name <you@example.com>
2020-10-23 15:36:05 -0400

    [1-trippler.js] finished trippler.js

--- 1-tripler.js
+++ 1-tripler.js
@@ -8,6 +8,28 @@ tripler([1,2,3]); // => [ 3, 6, 9 ]
 tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
 
+let array = [1, 2, 3, 4]
 function tripler(array) {
-  
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        let multiple = num * 3;
+        result.push(multiple);
+
+        // put back into results
+    };
+    // return a new array
+    // iterate through the array passed in
+    // multiply each element by 3
+    // push that element into my result
+    // return result
+
+
+    return result;
 }
+console.log(tripler(array))
+// understand the problem
+// what the problem is asking you to return
+// what do I need to solve this problem
+
+// what am I returning ? array
