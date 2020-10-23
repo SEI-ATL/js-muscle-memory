2a5fdad86f52c32548c85fb76e8112dc550ed9f5
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-10-23 12:36:57 -0700

    [1-triple.js] Complete 1st problem

--- 1-tripler.js
+++ 1-tripler.js
@@ -8,6 +8,19 @@ tripler([1,2,3]); // => [ 3, 6, 9 ]
 tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
 
+const numbers = [3, 6, 9];
+const result = [];
 function tripler(array) {
-  
+  let i = 0
+  for (let i = 0; i < array.length; i++) {
+      let num = array[i];
+      let multiple = num * 3;
+      result.push(multiple);
+  }
+  return result;
 }
+
+console.log(tripler(numbers));
+
+
+
